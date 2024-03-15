d162114fc12d031c748b1c7528018e664537645f
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-03-15 23:23:15 +0600

    Add picture and source file

--- src/component/shared/Navbar/Navbar.js
+++ src/component/shared/Navbar/Navbar.js
@@ -1,11 +1,23 @@
 
 import React from 'react';
+import logo from '@/assets/logo.png'
+import Image from 'next/image';
 
 const Navbar = () => {
-    
+
     return (
-        <div className="flex justify-between items-center">
-            <div className=''> logo</div>
+        <div className="flex justify-between items-center bg-purple-500 py-2">
+            <div className=''>
+                <Image
+                    alt='site_logo'
+                    src={logo}
+                    width={150}
+                    height={10}
+                    className='bg-red-500'
+                >
+
+                </Image>
+            </div>
             <div> Routes</div>
         </div>
     );
