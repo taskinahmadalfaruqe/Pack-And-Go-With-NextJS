201b97f161855e74fe87896d18c05d68cd843f22
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-03-22 00:10:17 +0600

    Add Travel Title

--- src/component/ui/TravelBenefit/TravelBenefit.js
+++ src/component/ui/TravelBenefit/TravelBenefit.js
@@ -1,9 +1,15 @@
+import CommonTitle from '@/component/shared/CommonTitle/CommonTitle';
 import React from 'react';
 
 const TravelBenefit = () => {
     return (
         <div>
-            Travel Benefit
+            <CommonTitle
+                SectionTitle={'Travel Benefit For User'}
+                SectionDescription={'We are self-service data analytics software that lets you create visually appealing data visualizations and insightful dashboards in minutes.'}
+            >
+
+            </CommonTitle>
         </div>
     );
 };
