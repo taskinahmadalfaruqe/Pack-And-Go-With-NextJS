3729baa1b463cee6f8bd3428a904f925b0657a6a
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-03-23 03:21:48 +0600

    Add Travel Benefit Section With Responsive

--- src/component/shared/CommonTitle/CommonTitle.js
+++ src/component/shared/CommonTitle/CommonTitle.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CommonTitle = ({SectionTitle, SectionDescription}) => {
     return (
-        <div className='flex gap-5 flex-col max-w-xl text-center mx-auto mt-10'>
+        <div className='flex gap-5 flex-col max-w-xl text-center mx-auto mt-14'>
             <p className='text-2xl font-semibold text-HeadingColor capitalize font-Truculenta'>{SectionTitle}</p>
             <p className='text-sm text-ParagraphColor'>{SectionDescription}</p>
         </div>
