92ffb174dcacfe10ee026e1eacb4d4767134d403
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-04-20 21:55:33 +0600

    Add New Section

---
+++ src/component/ui/MonthPromo/Month.js
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Month = () => {
+    return (
+        <div className='py-14'>
+            <div className='container'>
+                <Image></Image>
+            </div>
+        </div>
+    )
+}
+
+export default Month
