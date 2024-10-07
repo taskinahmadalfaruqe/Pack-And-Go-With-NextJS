3692129b833d61cb8b738888943120feade65eae
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-10-07 22:41:08 +0800

    Add A New Tour Card

--- src/component/utils/TopDestinationCard/TopDestinationCard.js
+++ src/component/utils/TopDestinationCard/TopDestinationCard.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
-const TopDestinationCard = () => {
-    return (
-        <div>
-            
-            
-        </div>
-    );
+const TopDestinationCard = ({ ImageLInk }) => {
+  return (
+    <div>
+      <div>
+        <Image
+          alt="Top Destination Image"
+          width={250}
+          height={250}
+          src={ImageLInk}
+          className="rounded-md"
+        ></Image>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default TopDestinationCard;
