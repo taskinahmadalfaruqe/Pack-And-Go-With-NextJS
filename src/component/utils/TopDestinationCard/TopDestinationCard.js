66d36529b81322e90953d059a16f8576bfb5f414
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-10-11 22:28:35 +0800

    Add A New Card

--- src/component/utils/TopDestinationCard/TopDestinationCard.js
+++ src/component/utils/TopDestinationCard/TopDestinationCard.js
@@ -13,7 +13,19 @@ const TopDestinationCard = ({ ImageLInk }) => {
           className="rounded-md"
         ></Image>
       </div>
-      <div></div>
+      <div>
+        <div>
+          <h2>Mount Agung, Bali</h2>
+        </div>
+        <div>
+          Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua minim veniam
+        </div>
+        <div>
+          <div>Location</div>
+          <div>Indonesia</div>
+        </div>
+      </div>
     </div>
   );
 };
